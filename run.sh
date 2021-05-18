4d8dd2d4d6f8013634fac5d5317e8960b470ab83
Rohan Awhad <rohanavad007@gmail.com>
2021-05-18 18:50:31 +0530

    uncommented train code in run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ export LR=0.00002
 
 export MODEL=$1
 
-# FOLD=0 python -m src.train
-# FOLD=1 python -m src.train
-# FOLD=2 python -m src.train
-# FOLD=3 python -m src.train
-# FOLD=4 python -m src.train
+FOLD=0 python -m src.train
+FOLD=1 python -m src.train
+FOLD=2 python -m src.train
+FOLD=3 python -m src.train
+FOLD=4 python -m src.train
 python -m src.predict
