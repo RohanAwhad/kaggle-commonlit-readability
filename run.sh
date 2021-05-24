d6e97d12903c838fd71a7b3ffc7ce215dfd35f49
Rohan Awhad <rohanavad007@gmail.com>
2021-05-24 07:03:21 +0000

    changed model script

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@ export TRAINING_DATA=input/train_folds.csv
 export TEST_DATA=input/test.csv
 
 export EPOCHS=5
-export BATCH_SIZE=4
+export BATCH_SIZE=16
 export LR=0.00002
 
 export MODEL=$1
 
 FOLD=0 python -m src.train
-FOLD=1 python -m src.train
-FOLD=2 python -m src.train
-FOLD=3 python -m src.train
-FOLD=4 python -m src.train
+#FOLD=1 python -m src.train
+#FOLD=2 python -m src.train
+#FOLD=3 python -m src.train
+#FOLD=4 python -m src.train
 python -m src.predict
--- src/model.py
+++ src/model.py
@@ -1,21 +1,40 @@
+import optuna
 import torch
-from tqdm import tqdm
-from transformers import AutoTokenizer, AutoModel
+from tqdm.notebook import tqdm
+from transformers import AutoConfig, AutoModel, AutoTokenizer
 
 
-class CommonLitBertBaseModel(torch.nn.Module):
-    def __init__(self, model, device="cpu"):
-        super(CommonLitBertBaseModel, self).__init__()
+class CommonLitBertModel(torch.nn.Module):
+    def __init__(self, 
+                 model_path=None, 
+                 tokenizer_path=None, 
+                 config_path=None, 
+                 device="cpu",
+                 max_len=512,
+                 dropout_rate=0
+        ):
+        super(CommonLitBertModel, self).__init__()
         self.device = device
-        self.tokenizer = AutoTokenizer.from_pretrained(model)
-        self.pre_trained = AutoModel.from_pretrained(model)
+        self.max_len = max_len
+        
+        if model_path is not None:
+            self.config = AutoConfig.from_pretrained(model_path)
+            self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+            self.pre_trained = AutoModel.from_pretrained(model_path)
+        else:
+            self.config = AutoConfig.from_pretrained(config_path)
+            self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, config=self.config)
+            self.pre_trained = AutoModel.from_config(self.config)
+            
+        self.dropout = torch.nn.Dropout(dropout_rate)
         self.linear = torch.nn.Linear(768, 1)
 
     def forward(self, text):
         inp = self.tokenizer(
-            text, return_tensors="pt", padding=True, truncation=True
+            text, return_tensors="pt", padding='longest', truncation=True, max_length=self.max_len
         ).to(self.device)
         x = self.pre_trained(**inp).pooler_output
+        x = self.dropout(x)
         x = self.linear(x)
         return x
 
@@ -27,8 +46,13 @@ class CommonLitBertBaseModel(torch.nn.Module):
         val_set=None,
         epochs=1,
         batch_size=1,
+        val_batch_size=None,
         model_path="model",
         shuffle=False,
+        patience=2,
+        scheduler=None,
+        save_model=True,
+        trial=None
     ):
 
         train_dataloader = torch.utils.data.DataLoader(
@@ -36,10 +60,12 @@ class CommonLitBertBaseModel(torch.nn.Module):
         )
 
         if val_set is not None:
+            val_batch_size = batch_size if val_batch_size is None else val_batch_size
             val_dataloader = torch.utils.data.DataLoader(
-                val_set, batch_size=batch_size, shuffle=shuffle
+                val_set, batch_size=val_batch_size, shuffle=shuffle
             )
 
+        prev_val_loss = None
         for epoch in range(1, epochs + 1):
             progress_bar = tqdm(
                 total=len(train_dataloader) + len(val_dataloader),
@@ -49,6 +75,7 @@ class CommonLitBertBaseModel(torch.nn.Module):
             # Training
             train_loss = []
             optimizer.zero_grad()
+            self.train()
             for data in train_dataloader:
                 loss = self.evaluate(data, criterion)
                 loss.backward()
@@ -61,6 +88,7 @@ class CommonLitBertBaseModel(torch.nn.Module):
 
             # Validation
             val_loss = []
+            self.eval()
             with torch.no_grad():
                 for data in val_dataloader:
                     loss = self.evaluate(data, criterion)
@@ -72,17 +100,44 @@ class CommonLitBertBaseModel(torch.nn.Module):
                         }
                     )
                     progress_bar.update()
-
-            torch.save(self.state_dict(), model_path + f"_{epoch}.pt")
+         
             progress_bar.close()
+            val_loss = sum(val_loss) / len(val_loss)
+            
+            trial.report(val_loss, epoch)
+            if trial.should_prune():
+                raise optuna.TrialPruned()
+
+            if scheduler is not None:
+                scheduler.step(val_loss)
+
+            if prev_val_loss is None or prev_val_loss > val_loss:
+                prev_val_loss = val_loss
+                countdown_patience = patience
+                print(f'Min val loss: {prev_val_loss} at Epoch: {epoch}')
+                if save_model:
+                    torch.save(self.state_dict(), f"{model_path}.pt")
+            else:
+                if countdown_patience == 0:
+                    break
+                else:
+                    countdown_patience -= 1
+
+        if save_model:
+            self.tokenizer.save_pretrained(f'tokenizers/{MODEL}')
+            self.config.save_pretrained(f'configs/{MODEL}')
+
+        return prev_val_loss
 
     def evaluate(self, data, criterion):
         text = data["text"]
         target = data["target"].to(self.device)
         y_pred = self.forward(text)
-        return criterion(y_pred, target)
+        # return criterion(y_pred, target)
+        return torch.sqrt(criterion(y_pred, target))
 
     def predict(self, data_seq, batch_size=16):
+        self.eval()
         with torch.no_grad():
             predictions = torch.empty((0, 1)).cpu()
             for i in tqdm(range(0, len(data_seq), batch_size), leave=False):
